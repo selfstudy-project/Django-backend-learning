88a0d5700dcbee16a52bf504b1a25597807844c2
racercpcode <convenient0610@gmail.com>
2022-03-27 14:42:33 +0800

    fix settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'racercpcode.herokuapp.com',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
